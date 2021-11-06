7ba3d85ba77d5406a0b4200b9af4b46284002fd8
Diego Cantelli <diegocantelli19@gmail.com>
2021-11-06 14:04:24 -0300

    Criacao do projeto FACADE - não implementado

---
+++ DesignPatterns.Facade/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns.Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fornece uma interface unificada para um conjunto de interfaces em um subsistema
+            // Define uma interface de nível superior que o torna o subsistema mais fácil de usar
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
