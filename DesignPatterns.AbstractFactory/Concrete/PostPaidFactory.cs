379a0db05e4717dd44d22a234b3bd029674e05f6
diegocantelli <diegocantelli19@gmail.com>
2022-10-04 10:54:03 -0300

    Implementando a classe concreta referente ao plano pos pago

---
+++ DesignPatterns.AbstractFactory/Concrete/PostPaidFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DesignPatterns.AbstractFactory.Interfaces;
+
+namespace DesignPatterns.AbstractFactory.Concrete
+{
+    public class PostPaidFactory : IMobilePlanFactory
+    {
+        public PostPaidFactory()
+        {
+        }
+
+        public IConnectionSpeedFactory CreateConnectionSpeed()
+        {
+            return new HighSpeed();
+        }
+
+        public IMobileDataFactory CreateMobileData()
+        {
+            return new UnlimitedMobileData();
+        }
+
+        public ITextMessageFactory CreateTextMessage()
+        {
+            return new UnlimitedTextMessage();
+        }
+    }
+}
