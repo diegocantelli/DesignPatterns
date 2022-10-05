29bb4ef5844d60805a652fc090f24882fc67cfdc
diegocantelli <diegocantelli19@gmail.com>
2022-10-05 11:23:40 -0300

    Implementando o padrao Prototype

---
+++ DesignPatterns.Prototype/CustomerPlan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace DesignPatterns.Prototype
+{
+    [Serializable]
+    public class CustomerPlan
+    {
+        public PlanVersion PlanVersion { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public List<string> CompanyPolicies { get; set; }
+        public List<string> Regulations { get; set; }
+        public List<string> Languages { get; set; }
+
+        public CustomerPlan()
+        {
+        }
+
+        public object DeepCopy()
+        {
+            using(var memoryStream = new MemoryStream())
+            {
+                var binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(memoryStream, this);
+                memoryStream.Position = 0;
+
+                return (CustomerPlan)binaryFormatter.Deserialize(memoryStream);
+            }
+        }
+    }
+
+    [Serializable]
+    public class PlanVersion
+    {
+        public int Version { get; set; }
+        public DateTime Creation { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+
+}
---
+++ DesignPatterns.Prototype/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DesignPatterns.Prototype
+{
+    //Este padrao foca em copiar objetos de forma eficiente atraves da tecnica DeepCopy
+    //Util quando se deseja criar multiplas instancias de um mesmo objeto, com um estado similar e apenas
+    //deseja-se alterar algumas propriedades
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerPlan = new CustomerPlan()
+            {
+                Name = "Plan version 1,",
+                Price = 100,
+                PlanVersion = new PlanVersion() { Version = 1 }
+            };
+
+            var customerPlanVersion2 = (CustomerPlan)customerPlan.DeepCopy();
+
+            customerPlanVersion2.Price = 200;
+
+            Console.WriteLine("Plan 1 " + customerPlan.Price);
+            Console.WriteLine("Plan 2 " + customerPlanVersion2.Price);
+
+            Console.ReadLine();
+        }
+    }
+}
